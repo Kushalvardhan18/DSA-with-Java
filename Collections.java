6f4dc629df1a52e31649b1135183715288766398
Kushal Vardhan <vardhank140102@gmail.com>
2025-08-01 12:49:08 +0530

    Lecture 1 Collections : Understanding the Basics of Collections

---
+++ Collections.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Collections {
+    public static void main(String[] args){
+
+        ArrayList<Integer>  list = new ArrayList<>();
+//        List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(2,5);
+        list.remove(1);
+
+//       for(int i=0;i<list.size();i++){
+//            System.out.println(list.get(i));
+//        }
+
+        Iterator<Integer> it = list.iterator();
+
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+        for(Integer x:list){
+            System.out.println("X : " + x);
+        }
+
+        System.out.println(list);
+
+
+        List<Integer> a = new LinkedList<>();
+        a.add(10);
+        a.add(20);
+        a.add(30);
+        a.add(2,40);
+        System.out.println(a);
+    }
+}
