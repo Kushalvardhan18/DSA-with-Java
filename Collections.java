d9aa6ed07fc9b6204a7402fcdb2e5cae9fe8033e
Kushal Vardhan <vardhank140102@gmail.com>
2025-09-24 15:28:05 +0530

    Hashmap

--- Collections.java
+++ Collections.java
@@ -1,29 +1,33 @@
 import java.util.*;
 
 public class Collections {
-    public static void main(String[] args){
-/*
-        ArrayList<Integer>  list = new ArrayList<>();
-//        List<Integer> list = new ArrayList<>();
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+//      List<Integer> list = new ArrayList<>();
 
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
-        list.add(2,5);
+        list.add(2, 5);
         list.remove(1);
 
-//       for(int i=0;i<list.size();i++){
-//            System.out.println(list.get(i));
-//        }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
 
+
+//       Iterate with help of Iterator
         Iterator<Integer> it = list.iterator();
 
-        while(it.hasNext()){
+        while (it.hasNext()) {
             System.out.println(it.next());
         }
 
-        for(Integer x:list){
+
+        // Iterate with help of enhanced For Loop
+        for (Integer x : list) {
             System.out.println("X : " + x);
         }
 
@@ -34,22 +38,20 @@ public class Collections {
         a.add(10);
         a.add(20);
         a.add(30);
-        a.add(2,40);
+        a.add(2, 40);
         System.out.println(a);
 
 
-        Stack <Integer> s = new Stack<>();
+        Stack<Integer> s = new Stack<>();
 
         s.push(1);
         s.push(20);
         s.push(56);
         s.push(40);
         System.out.println(s);
-        System.out.println("peek = "+ s.peek());
+        System.out.println("peek = " + s.peek());
         s.pop();
         System.out.println(s);
-
-        */
 
 
         Queue<Integer> q = new LinkedList<>();
@@ -63,5 +65,17 @@ public class Collections {
         System.out.println(q.poll());
         System.out.println(q.remove());
 
+
+        Map<Integer, String> student = new HashMap<>();
+
+        student.put(1, "kushal");
+        student.put(2, "aman");
+        student.put(3, "vardhan");
+        student.put(4, "kvvvv");
+
+        student.putIfAbsent(4, "kv");
+
+
+        System.out.println(student);
     }
 }
