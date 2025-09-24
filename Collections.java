21f7e4ae2d1f42691803730373127a35b447727e
Kushal Vardhan <vardhank140102@gmail.com>
2025-09-24 15:45:24 +0530

    deque and treemap

--- Collections.java
+++ Collections.java
@@ -65,17 +65,56 @@ public class Collections {
         System.out.println(q.poll());
         System.out.println(q.remove());
 
+Deque<Integer> dq = new LinkedList<>();
 
+dq.addFirst(20);
+dq.addLast(11);
+dq.offer(20);
+dq.poll();
+dq.offer(25);
+        System.out.println("Deque"+dq);
         Map<Integer, String> student = new HashMap<>();
 
         student.put(1, "kushal");
+        student.put(4, "kvvvv");
         student.put(2, "aman");
         student.put(3, "vardhan");
-        student.put(4, "kvvvv");
 
         student.putIfAbsent(4, "kv");
 
 
         System.out.println(student);
+
+        for(Integer rollno :student.keySet()){
+            System.out.println(rollno);
+        }for(String name :student.values()){
+            System.out.println(name);
+        }
+
+        for(Map.Entry<Integer,String> st : student.entrySet()){
+            System.out.println("key : "+st.getKey()+ " , "+"value : "+st.getValue());
+        }
+
+        Map<Integer, String> student2 = new TreeMap<>();
+
+        student2.put(1, "kushal");
+        student2.put(4, "kvvvv");
+        student2.put(2, "aman");
+        student2.put(3, "vardhan");
+
+        student2.putIfAbsent(4, "kv");
+
+
+        System.out.println(student2);
+
+        for(Integer rollno :student2.keySet()){
+            System.out.println(rollno);
+        }for(String name :student2.values()){
+            System.out.println(name);
+        }
+
+        for(Map.Entry<Integer,String> st : student2.entrySet()){
+            System.out.println("key : "+st.getKey()+ " , "+"value : "+st.getValue());
+        }
     }
 }
