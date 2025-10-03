d84455ed10ea4ec7028c10f2a9f383c940eb30c9
Kushal Vardhan <vardhank140102@gmail.com>
2025-10-03 12:01:33 +0530

    custom equals function

---
+++ Collections2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Employee {
+    public int id;
+    public String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+        return id == employee.id && Objects.equals(name, employee.name);
+    }
+
+
+}
+
+public class Collections2 {
+    public static void main(String[] args) {
+        int a = 2, b = 2;
+        System.out.println(a == b);
+
+        Integer c=2,d=2;
+        System.out.println(c==d);
+
+
+        Employee e1 = new Employee(1, "aman");
+        Employee e2 = new Employee(1, "aman");
+        System.out.println(e1 == e2);
+        System.out.println(e1.equals(e2));
+    }
+}
